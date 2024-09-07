7aa0512c9a7823c3d0259efc3e4525970934310b
madoha <madoha00@gmail.com>
2024-09-08 01:32:07 +0500

    added dtos

--- GogoLibrary.Application/obj/Debug/net8.0/GogoLibrary.Application.AssemblyInfo.cs
+++ GogoLibrary.Application/obj/Debug/net8.0/GogoLibrary.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GogoLibrary.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9765c664995bffde0aab7f29d9b7fc3b0145dd8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac601613a1233c35e0ea3b71524ff45b0013e46")]
 [assembly: System.Reflection.AssemblyProductAttribute("GogoLibrary.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GogoLibrary.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
